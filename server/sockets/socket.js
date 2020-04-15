21c3986e5f696146529b0dd1846e985eb4770760
Jose Coucouvinos <jecucu@gmail.com>
2020-04-14 22:23:02 -0300

    Primer commit

---
+++ server/sockets/socket.js
@@ -0,0 +1,45 @@
+let { io } = require('../server');
+
+io.on('connection', (client) => {
+    console.log('Usuario conectado');
+
+    client.emit('enviarMensaje', {
+        usuario: 'Administrador',
+        mensaje: 'Bienvenido a la aplicacion'
+    });
+
+    client.on('disconnect', () => {
+        console.log('Usuario desconectado');
+    });
+
+    // Escuchar cliente
+    client.on('enviarMensaje', (data, callback) => {
+        console.log(data);
+
+
+        client.broadcast.emit('enviarMensaje', data);
+
+        // if (mensaje.usuario) {
+        //     callback({
+        //         resp: 'Todo salio bien'
+        //     });
+        // } else {
+        //     callback({
+        //         resp: 'Todo salio mal'
+        //     });
+        // }
+
+    });
+
+});
+
+
+// mensaje  para usar desde el navegador
+/*
+
+socket.emit('enviarMensaje', {
+    usuario: 'Jose',
+    mensaje: 'Hola mundo'
+});
+
+*/
